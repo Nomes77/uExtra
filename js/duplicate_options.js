ac6d548f96c57430842967532d68cbf55305e48c
Nomes77 <80090789+Nomes77@users.noreply.github.com>
2024-01-13 08:17:58 +0000

    Improve Duplicate Tab (#1)

---
+++ js/duplicate_options.js
@@ -0,0 +1,25 @@
+const duplicate_options = {
+  background: false,
+};
+document.addEventListener('DOMContentLoaded', async () => {
+  const commands = await browser.commands.getAll();
+  for (const command of commands) {
+    const shortcut = document.getElementById(`${command.name}-shortcut`);
+    if (!shortcut) {
+      continue;
+    }
+    shortcut.textContent = command.shortcut || 'not set';
+  }
+  const options = await browser.storage.sync.get(duplicate_options);
+  const background = document.getElementById('background');
+  background.checked = options.background;
+  background.addEventListener('change', () =>
+    browser.storage.sync.set({
+      background: background.checked,
+    }),
+  );
+
+  if (await browser.extension.isAllowedIncognitoAccess()) {
+    document.getElementById('allow-in-incognito-notice').style.display = 'none';
+  }
+});
